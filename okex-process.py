7e6b10e2318626394bd756c4e2445831971d476d
user <ersan@solist.ru>
2021-03-14 14:13:39 +0300

    парсинг в okex-process, перевод в int, float, timestamp

--- okex-process.py
+++ okex-process.py
@@ -2,13 +2,26 @@
 
 import socket
 import zlib
-import sys
+import json
+from datetime import datetime
+import pytz
 
 
 def process_message(message):
     decompress = zlib.decompressobj(-zlib.MAX_WBITS)
     dec_messge = decompress.decompress(message)
-    message_out(dec_messge)
+    data_dct = json.loads(dec_messge.decode())
+    if 'data' in data_dct:
+        data = data_dct['data'][0]
+        data['stream'] = data_dct['table'] + ':' + data.pop('instrument_id')
+        data['id'] = int(data.pop('trade_id'))
+        data['amount'] = float(data.pop('size'))
+        data['price'] = float(data['price'])
+        data['exchange'] = 'OKEX'
+        data['exchange_time'] = datetime.fromisoformat(data.pop('timestamp')[:-1]+'+00:00')
+        data['local_time'] = datetime.now(tz=pytz.timezone('Europe/Moscow'))
+        message_out(data)
+        return
 
 
 def message_out(message):
